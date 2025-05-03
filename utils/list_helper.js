0b5a029c82c5b1355a65625164c4329c326b4844
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2025-05-03 15:26:03 +0100

    test commit

---
+++ utils/list_helper.js
@@ -0,0 +1,36 @@
+
+  const dummy = (blogs) => {
+    return 1;
+  };
+  
+  const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  };
+  
+  const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+      return null;
+    }
+  
+    const authorLikes = {};
+  
+    blogs.forEach((blog) => {
+      authorLikes[blog.author] = (authorLikes[blog.author] || 0) + blog.likes;
+    });
+  
+    const topAuthor = Object.keys(authorLikes).reduce((maxAuthor, author) => {
+      return authorLikes[author] > authorLikes[maxAuthor] ? author : maxAuthor;
+    });
+  
+    return {
+      author: topAuthor,
+      likes: authorLikes[topAuthor],
+    };
+  };
+  
+  module.exports = {
+    dummy,
+    totalLikes,
+    mostLikes,
+  };
+  
